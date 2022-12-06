c09a211e377309a9c497701c107088026e5cc6e9
Mark Caggiano <mcisback@gmail.com>
2022-12-06 15:30:18 +0100

    fix bulk domain last domain missing bug

--- resources/js/Pages/Helpers/intervalLoop.js
+++ resources/js/Pages/Helpers/intervalLoop.js
@@ -3,18 +3,18 @@ export function intervalLoop(records, callback, time) {
     let count = 0
 
     const interval = setInterval(() => {
-        if(count >= (records.length - 1)) {
-            console.log('records: ', records)
-
-            clearInterval(interval)
-        } else {
-            console.log(`records[${count}]: `, records[count])
+        if(count < records.length) {
+            console.log(`${records.length},records[${count}]: `, records[count])
 
             if(!!records[count]) {
                 callback(records[count])
             }
 
             count++
+        } else {
+            console.log('records: ', records)
+
+            clearInterval(interval)
         }
     }, time)
 }
