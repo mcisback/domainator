180126efaee18e9d271e4c351fda27947b389c1b
Mark <mcisback@gmail.com>
2023-04-25 22:22:25 +0200

    master | feat(ToggableArrow not working)

---
+++ resources/js/Pages/Components/Widgets/ToggableArrow.svelte
@@ -0,0 +1,33 @@
+<script>
+
+    import { createEventDispatcher } from 'svelte';
+
+    export let toggled = false
+
+    const dispatch = createEventDispatcher();
+
+    function dispatchToggled() {
+        toggled = !toggled
+
+        console.log(`toggableArrowClicked: ${toggled}`)
+
+        dispatch('toggableArrowClicked', {
+            toggled
+        });
+    }
+
+</script>
+
+<div class="togglable-arrow" on:click={() => dispatchToggled()}>
+    {#if !toggled}
+        <i class="fa-solid fa-caret-down"></i>
+    {:else}
+        <i class="fa-solid fa-caret-up"></i>
+    {/if}
+</div>
+
+<style>
+    .togglable-arrow {
+        cursor: pointer;
+    }
+</style>
